d5c0d1150493314e057165c455c49635a1663371
AdityaGupta1 <adityag@gmail.com>
2025-01-13 22:50:12 -0800

    add photos

--- assets/js/swag.js
+++ assets/js/swag.js
@@ -9,6 +9,38 @@ function selectSection(id) {
 	$(`#nav-${id}`).addClass('active');
 }
 
+function setPortfolioVisible(visible) {
+	document.getElementById('photo-portfolio-grid').style.display = visible ? '' : 'none';
+}
+
+function setAlbumVisible(targetId) {
+	Array.from(document.getElementsByClassName('photo-album-grid')).forEach(element => {
+		element.style.display = element.id === targetId ? '' : 'none';
+	});
+}
+
+function setAlbumBackVisibility(visible) {
+	Array.from(document.getElementsByClassName('photo-album-back')).forEach(element => {
+		element.style.display = visible ? '' : 'none';
+	});
+}
+
+function activateAlbum(albumName) {
+	targetId = albumName + "-photos";
+
+	setAlbumVisible(targetId);
+	setAlbumBackVisibility(true);
+	setPortfolioVisible(false);
+}
+
+function deactivateAlbum() {
+	setAlbumVisible('');
+	setAlbumBackVisibility(false);
+	setPortfolioVisible(true);
+}
+
+deactivateAlbum();
+
 document.addEventListener('DOMContentLoaded', function() {
 	let hash = window.location.hash;
 	let section = hash ? hash.substring(1) : 'projects';
