45125caef406ca4d85a0fa7a5ba7e401366b3712
AdityaGupta1 <adityag@gmail.com>
2025-01-14 12:16:18 -0800

    add permalinks to photo albums

--- assets/js/swag.js
+++ assets/js/swag.js
@@ -1,55 +1,86 @@
 function selectSection(id) {
-	$('#main > section').addClass('inactive');
-	$(`#${id}`).removeClass('inactive');
+    $('#main > section').addClass('inactive');
+    $(`#${id}`).removeClass('inactive');
 
-	window.location.hash = `#${id}`;
-	window.scrollTo(0,0);
+    window.location.hash = `#${id}`;
+    window.scrollTo(0,0);
 
-	$('#nav').find('a').removeClass('active');
-	$(`#nav-${id}`).addClass('active');
+    $('#nav').find('a').removeClass('active');
+    $(`#nav-${id}`).addClass('active');
 }
 
 function setPortfolioVisible(visible) {
-	visibilityStyleText = visible ? '' : 'none';
-	document.getElementById('photo-portfolio-grid').style.display = visibilityStyleText;
-	document.getElementById('photography-click-advice').style.display = visibilityStyleText;
+    visibilityStyleText = visible ? '' : 'none';
+    document.getElementById('photo-portfolio-grid').style.display = visibilityStyleText;
+    document.getElementById('photography-click-advice').style.display = visibilityStyleText;
 }
 
 function setAlbumVisible(targetId) {
-	Array.from(document.getElementsByClassName('photo-album-grid')).forEach(element => {
-		element.style.display = element.id === targetId ? '' : 'none';
-	});
+    Array.from(document.getElementsByClassName('photo-album-grid')).forEach(element => {
+        element.style.display = element.id === targetId ? '' : 'none';
+    });
 }
 
 function setAlbumBackVisibility(visible) {
-	Array.from(document.getElementsByClassName('photo-album-back')).forEach(element => {
-		element.style.display = visible ? '' : 'none';
-	});
+    Array.from(document.getElementsByClassName('photo-album-back')).forEach(element => {
+        element.style.display = visible ? '' : 'none';
+    });
 }
 
 function activateAlbum(albumName) {
-	targetId = albumName + "-photos";
+    targetId = albumName + "-photos";
 
-	setAlbumVisible(targetId);
-	setAlbumBackVisibility(true);
-	setPortfolioVisible(false);
+    if (!document.getElementById(targetId)) {
+        return;
+    }
+
+    setAlbumVisible(targetId);
+    setAlbumBackVisibility(true);
+    setPortfolioVisible(false);
+
+    $('html, body').animate({ scrollTop: 0 }, 'smooth');
+
+    window.location.hash = `#photography_${albumName}`;
 }
 
-function deactivateAlbum() {
-	setAlbumVisible('');
-	setAlbumBackVisibility(false);
-	setPortfolioVisible(true);
+function deactivateAlbum(setHash = true) {
+    setAlbumVisible('');
+    setAlbumBackVisibility(false);
+    setPortfolioVisible(true);
+
+    if (setHash) {
+        window.location.hash = '#photography';
+    }
 }
 
-deactivateAlbum();
+deactivateAlbum(false);
 
 document.addEventListener('DOMContentLoaded', function() {
-	let hash = window.location.hash;
-	let section = hash ? hash.substring(1) : 'projects';
+    let section = 'projects';
+    let subsection = '';
 
-	if (!$('#main > section').map(function() { return this.id; }).get().includes(section)) {
-		section = 'projects';
-	}
+    const hash = window.location.hash;
+    if (hash) {
+        const split = hash.substring(1).split('_');
+        section = split[0];
+        if (split.length > 1) {
+            subsection = split[1];
+        }
+    }
 
-	selectSection(section);
+    if (!$('#main > section').map(function() { return this.id; }).get().includes(section)) {
+        section = 'projects';
+    }
+
+    selectSection(section);
+
+    switch (section) {
+        case 'photography':
+            activateAlbum(subsection);
+            break;
+        default:
+            break;
+    }
+
+    document.getElementById('main-flex').style = ''; // show all content only after properly setting section
 });
