b367ecd3683131de5561f95cec2748694d4d5d7d
AdityaGupta1 <adityag@gmail.com>
2026-04-11 13:13:32 -0700

    some fixes or something, idk this is kinda stupid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,8 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://adityag1.com',
   output: 'static',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
 });
